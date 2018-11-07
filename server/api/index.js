81c5f2b62df40da8e4a0e56e6a32af5843652583
daspete <daspetemail@gmail.com>
2018-11-07 21:58:28 +0100

    updated api modules

--- server/api/index.js
+++ server/api/index.js
@@ -1,18 +1,12 @@
-const fs = require('fs')
-const path = require('path')
-
 const express = require('express');
 const router = express.Router();
 
-const modulePath = path.resolve('modules')
-const moduleFolders = fs.readdirSync(modulePath)
 
-moduleFolders.forEach((_module) => {
-    let moduleFolderName = _module;
-    let moduleName = _module.toLowerCase()
+const modules = process.env.ENABLED_MODULES.split(',')
 
+modules.forEach((moduleName) => {
     try {
-        router.use(`/${ moduleName }`, require('@modules/' + moduleFolderName + '/api/routes.js'))
+        router.use(`/${ moduleName }`, require('@modules/' + moduleName + '/api/routes.js'))
     }catch(e){}
 })
 
