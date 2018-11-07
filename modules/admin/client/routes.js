8eb45b969d08be6e252e82b92cc023fec5fc4d2d
daspete <daspetemail@gmail.com>
2018-11-07 15:49:21 +0100

    added dynamic module admin routes

--- modules/admin/client/routes.js
+++ modules/admin/client/routes.js
@@ -1,18 +1,23 @@
 import AdminIndex from '~admin/pages/index.vue'
 
-// import UsersAdminIndex from '~users/pages/admin/index.vue'
+const adminRoutes = []
+
+const modules = process.env.ENABLED_MODULES.split(',')
+
+const context = require.context('~/../modules', true, /adminroutes\.js$/)
+context.keys().forEach((key) => {
+    let moduleName = key.substr(2, key.length - 1).split('/')[0]
+
+    if(modules.contains(moduleName)){
+        adminRoutes.push(context(key).default)
+    }
+})
 
 export default [
     {
         path: '/admin',
         name: 'admin_index',
         component: AdminIndex,
-        children: [
-            {
-                path: 'users',
-                name: 'admin_users_index',
-                component: require('~users/pages/admin/index.vue').default
-            }
-        ]
+        children: adminRoutes
     }
 ]
